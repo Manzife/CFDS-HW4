9c8e9d3fd6c20045737bf1a33552a63c80b4852c
manzife <felipemanzife@gmail.com>
2025-10-21 17:54:22 +0200

    - Bug fix to work the predction

--- src/best_library/model.py
+++ src/best_library/model.py
@@ -11,7 +11,12 @@ def train_model(X_train, y_train, model_type='logistic'):
     model.fit(X_train, y_train)
     return model
 
-def predict(model, X_train, X_test):
-    X_train['predictions'] = model.predict_proba(X_train)[:, 1]
-    X_test['predictions'] = model.predict_proba(X_test)[:, 1]
-    return X_train, X_test
+def predict(model, train_df, test_df, features):
+    """Add prediction probabilities to both train and test sets."""
+    train_df = train_df.copy()
+    test_df = test_df.copy()
+
+    train_df['predictions'] = model.predict_proba(train_df[features])[:, 1]
+    test_df['predictions'] = model.predict_proba(test_df[features])[:, 1]
+
+    return train_df, test_df
